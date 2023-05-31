a2a8a1570eb156fe9e195e40f993d8d21de5ae98
Fabian Saavedra <fabian.saavedra01@uptc.edu.co>
2023-05-31 15:23:44 -0500

    Add new funcionality

--- Token.java
+++ Token.java
@@ -1,17 +1,123 @@
-class Token{
-    private byte init;
-    private byte accesControl;
-    private byte end;
+class Token implements Cloneable{
+    private String init;
+    private String accesControl;
+    private String end;
+
+    private String frameControl;
+    private String destinatioAddress;
+    private String sourceAddres;
+    private String data;
+    private String frameStatus;
+
 
     public Token(){
-        init = 1;
-        accesControl = 1;
-        end = 1;
+        cleanToken();
+    }
+
+
+    public void addMessage( String frameControl, String sourceAddres , String destinatioAddress, String data, String frameStatus){
+        this.accesControl = "1";
+        this.frameControl = frameControl;
+        this.destinatioAddress = destinatioAddress;
+        this.sourceAddres = sourceAddres;
+        this.data = data;
+        this.frameStatus = frameStatus;
+    }
+
+    public void cleanToken(){
+        this.init = "1";
+        this.accesControl = "0";
+        this.end = "1";
+        this.frameControl = "0";
+        this.destinatioAddress ="0";
+        this.sourceAddres ="0";
+        this.data = "0";
+        this.frameStatus ="0";
     }
     
+    public String getInit() {
+        return init;
+    }
+
+    public void setInit(String init) {
+        this.init = init;
+    }
+
+    public String getAccesControl() {
+        return accesControl;
+    }
+
+    public void setAccesControl(String accesControl) {
+        this.accesControl = accesControl;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getFrameControl() {
+        return frameControl;
+    }
+
+    public void setFrameControl(String frameControl) {
+        this.frameControl = frameControl;
+    }
+
+    public String getDestinatioAddress() {
+        return destinatioAddress;
+    }
+
+    public void setDestinatioAddress(String destinatioAddress) {
+        this.destinatioAddress = destinatioAddress;
+    }
+
+    public String getSourceAddres() {
+        return sourceAddres;
+    }
+
+    public void setSourceAddres(String sourceAddres) {
+        this.sourceAddres = sourceAddres;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getFrameStatus() {
+        return frameStatus;
+    }
+
+    public void setFrameStatus(String frameStatus) {
+        this.frameStatus = frameStatus;
+    }
+
     @Override
     public String toString() {
+        if(accesControl.equals("0"))
         return "[ "+ init +", "+ accesControl +", "+end+" ]";
+        else 
+        return "[ "
+        + init +", "+ accesControl +", "+frameControl +", " +destinatioAddress +", "+sourceAddres +", "+ data +", "+ end+ ", "+frameStatus+"]";
     }
+
+    @Override
+    protected Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
 }
--- TokenRingSimulation.java
+++ TokenRingSimulation.java
@@ -9,11 +9,16 @@ public class TokenRingSimulation extends JFrame {
     private JButton stopButton;
     private JButton resetButton;
     private JTextField numHostsTextField;
+    private JTextField messageTextField;
+    private JTextField senderTextField;
+    private JTextField receiverTextField;
     private JPanel tokenPanel;
     private JPanel statusPanel;
     private JPanel mainPanel; 
     private JTextArea statusTextArea;
     private Token token;
+    private Token messagetoken;
+    private boolean thereIsMessage;
  
     
     public TokenRingSimulation() {
@@ -23,6 +28,9 @@ public class TokenRingSimulation extends JFrame {
         stopButton = new JButton("Detener");
         resetButton = new JButton("Reiniciar");
         numHostsTextField = new JTextField(5);
+        messageTextField = new JTextField(20);
+        senderTextField = new JTextField(5);
+        receiverTextField = new JTextField(5);
         tokenPanel = new JPanel();
         
         JPanel buttonPanel = new JPanel();
@@ -32,16 +40,26 @@ public class TokenRingSimulation extends JFrame {
         buttonPanel.add(stopButton);
         buttonPanel.add(resetButton);
 
+        JPanel messagePanel = new JPanel();
+        messagePanel.setLayout(new GridLayout(8, 1));
+        messagePanel.add(new JLabel("Mensaje a enviar:"));
+        messagePanel.add(messageTextField);
+        messagePanel.add(new JLabel("Emisor:"));
+        messagePanel.add(senderTextField);
+        messagePanel.add(new JLabel("Receptor:"));
+        messagePanel.add(receiverTextField);
+
         
         
         mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
         mainPanel.add(tokenPanel, BorderLayout.WEST);
         mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        mainPanel.add(messagePanel,  BorderLayout.EAST);
         
         getContentPane().add(mainPanel);
         
-        setSize(500, 300);
+        setSize(700, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         
@@ -59,10 +77,15 @@ public class TokenRingSimulation extends JFrame {
                 });
                 
                 tokenRing.start();
+                thereIsMessage = false;
+                adminMessage();
                 startButton.setEnabled(false);
                 stopButton.setEnabled(true);
                 resetButton.setEnabled(true);
                 numHostsTextField.setEnabled(false);
+                messageTextField.setEnabled(false);
+                senderTextField.setEditable(false);
+                receiverTextField.setEditable(false);
             }
         });
         
@@ -73,6 +96,9 @@ public class TokenRingSimulation extends JFrame {
                 stopButton.setEnabled(false);
                 resetButton.setEnabled(true);
                 numHostsTextField.setEnabled(true);
+                messageTextField.setEnabled(true);
+                senderTextField.setEditable(true);
+                receiverTextField.setEditable(true);
             }
         });
         
@@ -83,7 +109,9 @@ public class TokenRingSimulation extends JFrame {
                 stopButton.setEnabled(false);
                 resetButton.setEnabled(false);
                 numHostsTextField.setEnabled(true);
-                
+                messageTextField.setEnabled(true);
+                senderTextField.setEditable(true);
+                receiverTextField.setEditable(true);
                 clearTokenPanel();
             }
         });
@@ -128,11 +156,56 @@ public class TokenRingSimulation extends JFrame {
         JLabel currentLabel = (JLabel) components[currentHost];
         currentLabel.setForeground(Color.RED);
 
-        statusTextArea.append("Pasó el token al Host " + currentHost + "\n");
-        //aquí debe estar lo del token con lo del mensaje 
         statusTextArea.append("Token: "+ token.toString() +"\n");
+        statusTextArea.append("Pasó el token al Host " + currentHost + "\n"); 
+        this.adminToken(currentHost);
+    }
+
+    private void adminMessage(){
+        if(!messageTextField.getText().equals("")){
+            messagetoken = new Token();
+            messagetoken.addMessage(this.changeNumberToBinary(0,1), this.changeNumberToBinary(Integer.valueOf(this.senderTextField.getText()),8),
+            this.changeNumberToBinary(Integer.valueOf(this.receiverTextField.getText()),8), 
+            this.changeStringToBinary(this.messageTextField.getText()), this.changeNumberToBinary(0,1));
+            thereIsMessage = true;
+        }else{
+            token.cleanToken();
+        }
+    }
+
+    private void adminToken(int currentHost){
+        if(thereIsMessage)
+            if(messagetoken.getSourceAddres().equals(this.changeNumberToBinary(currentHost,8))){
+                token =  ((Token) messagetoken.clone());
+                messagetoken.cleanToken();
+            }else if(token.getDestinatioAddress().equals(this.changeNumberToBinary(currentHost,8))){
+                statusTextArea.append("El host" + currentHost + " copia el contenido del token para procesar\n"); 
+                token.setFrameStatus(this.changeNumberToBinary(1,1));
+            }else if(token.getSourceAddres().equals(this.changeNumberToBinary(currentHost,8)) ){
+                statusTextArea.append("El host " + currentHost + " verifica envio, limpia el token\n"); 
+                token.cleanToken();
+            }
+    }
+
+    private String changeNumberToBinary(int numero, int lenght){
+        String binario = String.format("%"+lenght+"s", Integer.toBinaryString(numero)).replace(' ', '0');
+        return binario;
+    }
+
+    private String changeStringToBinary(String cadena){
+        StringBuilder binario = new StringBuilder();
+
+        byte[] bytes = cadena.getBytes();
+        for (byte b : bytes) {
+            for (int i = 7; i >= 0; i--) {
+                binario.append((b >> i) & 1);
+            }
+        }
+
+        return binario.toString();
     }
     
+
     private void clearTokenPanel() {
         tokenPanel.removeAll();
         tokenPanel.revalidate();
